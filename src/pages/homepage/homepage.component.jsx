771022342e1f165e9c764d1967d2ca0462743395
SuperBear <superbear1212@gmail.com>
2021-01-24 17:26:09 -0500

    finished styling of homepage, separete homepagen into small components

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import "./homepage.styles.scss"
+import '../../components/directory/directoy.component'
+import Directory from "../../components/directory/directoy.component"
+
+const HomePage=()=>(
+    <div className="homepage">
+        <Directory />
+    </div>
+)
+
+export default HomePage
