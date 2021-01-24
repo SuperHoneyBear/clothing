0b478ead8fbd0525d6abca7d9cb601ab914d3bbe
SuperBear <superbear1212@gmail.com>
2021-01-24 15:48:57 -0500

    change back to original files

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -20,7 +20,7 @@ const HomePage=()=>(
             <div className="menu-item">
                 <div className="content">
                     <h1 className="title">SNEAKERS</h1>
-                    <span className="subtitle">SHOP Latter</span>
+                    <span className="subtitle">SHOP NOW</span>
                 </div>
             </div>
         
